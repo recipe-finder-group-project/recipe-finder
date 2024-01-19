9b3e10af54f171ddf33130a6be533e489adfb03d
ervette <kiriltalalayko@gmail.com>
2024-01-19 13:48:00 +0000

    minor changes

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -8,7 +8,7 @@ const getRecipes = async (req, res) => {
   res.status(200).json(recipes)
 }
 
-// get a single recipe by id 
+// get a single recipe by id
 /// later on i will change it to the name for convenience
 const getRecipe = async (req, res) => {
   const { id } = req.params
@@ -55,16 +55,49 @@ const createRecipe = async (req, res) => {
   }
 }
 
-// delete a recipe
-const deleteRecipe = async (req, res) => {}
+// delete a recipe by id
+const deleteRecipe = async (req, res) => {
+  const { id } = req.params
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "No such recipe" })
+  }
+
+  const recipe = await Recipe.findOneAndDelete({ _id: id })
+
+  if (!recipe) {
+    return res.status(400).json({ error: "No such recipe" })
+  }
+
+  res.status(200).json(recipe)
+}
 
 // update a recipe
-const updateRecipe = async (req, res) => {}
+const updateRecipe = async (req, res) => {
+  const { id } = req.params
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "No such recipe" })
+  }
+
+  const recipe = await Recipe.findOneAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
+    }
+  )
+
+  if (!recipe) {
+    return res.status(400).json({ error: "No such recipe" })
+  }
+
+  res.status(200).json(recipe)
+}
 
 module.exports = {
-    getRecipes,
-    getRecipe,
-    createRecipe,
-    deleteRecipe,
-    updateRecipe
+  getRecipes,
+  getRecipe,
+  createRecipe,
+  deleteRecipe,
+  updateRecipe,
 }
