5805b37ca82bf67eb177a1d0d086f08931d5bf57
ervette <kiriltalalayko@gmail.com>
2024-01-19 13:12:17 +0000

    Created working routes to fetch data from database

---
+++ server/controllers/recipeController.js
@@ -0,0 +1,69 @@
+const Recipe = require("../models/recipeModel")
+const mongoose = require("mongoose")
+
+//get all recipes
+const getRecipes = async (req, res) => {
+  const recipes = await Recipe.find({}).sort({ createdAt: -1 })
+
+  res.status(200).json(recipes)
+}
+
+// get a single recipe
+const getRecipe = async (req, res) => {
+  const { id } = req.params
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such recipe" })
+  }
+
+  const recipe = await Recipe.findById(id)
+
+  if (!recipe) {
+    return res.status(404).json({ error: "No such recipe" })
+  }
+
+  res.status(200).json(recipe)
+}
+
+//create new recipe
+const createRecipe = async (req, res) => {
+  const {
+    name,
+    dietType,
+    mealCategory,
+    preparationTime,
+    difficulty,
+    ingredients,
+    reviews,
+  } = req.body
+
+  // add to the database
+  try {
+    const recipe = await Recipe.create({
+      name,
+      dietType,
+      mealCategory,
+      preparationTime,
+      difficulty,
+      ingredients,
+      reviews,
+    })
+    res.status(200).json(recipe)
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+}
+
+// delete a recipe
+const deleteRecipe = async (req, res) => {}
+
+// update a recipe
+const updateRecipe = async (req, res) => {}
+
+module.exports = {
+    getRecipes,
+    getRecipe,
+    createRecipe,
+    deleteRecipe,
+    updateRecipe
+}
--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -1,37 +1,27 @@
 const express = require("express")
-const Recipe = require("../models/recipeModel")
+const {
+    getRecipes,
+    getRecipe,
+    createRecipe,
+    deleteRecipe,
+    updateRecipe
+} = require('../controllers/recipeController')
 
 const router = express.Router()
 
 // GET all recipes
-router.get("/", (req, res) => {
-  res.json({ mssg: "GET all recipes" })
-})
+router.get("/", getRecipes)
 
 // GET a single recipe
-router.get("/:id", (req, res) => {
-  res.json({ mssg: "GET a single recipe" })
-})
+router.get("/:id", getRecipe)
 
 // POST a new recipe
-router.post("/", async (req, res) => {
-  const { name, dietType, mealCategory, preparationTime, difficulty, ingredients, reviews} = req.body
+router.post("/", createRecipe)
 
-  try {
-    const recipe = await Recipe.create({ name, dietType, mealCategory, preparationTime, difficulty, ingredients, reviews})
-    res.status(200).json(recipe)
-  } catch (error) {
-    res.status(400).json({ error: error.message })
-  }
-})
 // DELETE a recipe
-router.delete('/:id', (req, res) => {
-    res.json({mssg: 'DELETE a recipe'})
-  })
-  
-  // UPDATE a recipe
-  router.patch('/:id', (req, res) => {
-    res.json({mssg: 'UPDATE a recipe by id'})
-  })
-  
-  module.exports = router
+router.delete('/:id', deleteRecipe)
+
+// UPDATE a recipe
+router.patch('/:id', updateRecipe)
+
+module.exports = router
