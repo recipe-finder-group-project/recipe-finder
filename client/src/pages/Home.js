6c58239ec1495e3c6cbd5e25ce705775200ad4c9
ervette <kiriltalalayko@gmail.com>
2024-01-26 14:10:58 +0000

    finished basic main page + data fetching set-up

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from "react"
+import ReviewCard from "../components/ReviewCard"
+import '../index.css'
 
 const Home = () => {
-    return (
-        <div className="home">
-            <h2>Home</h2>
-        </div>
-    )
+  const [recipes, setRecipes] = useState(null)
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      const response = await fetch("/api/recipes")
+      const json = await response.json()
+
+      if (response.ok) {
+        setRecipes(json)
+      }
+    }
+    fetchRecipes()
+  }, [])
+
+  return (
+    <div className="home">
+      <div className="recipes">
+        {recipes &&
+          recipes.map((recipe) => (
+            <React.Fragment key={recipe._id}>
+              <h2>{recipe.name}</h2>
+              <p>Diet Type - {recipe.dietType}</p>
+              <p>Meal Category - {recipe.mealCategory}</p>
+              <p>Preparation time - {recipe.preparationTime} min</p>
+              <p>Difficulty - {recipe.difficulty}*</p>
+              <p>Ingredients : </p>
+              <ul>
+                {recipe.ingredients.map((ingredient, index) => (
+                  <li key={index}>{ingredient}</li>
+                ))}
+              </ul>
+              <div>
+                <ReviewCard reviews={recipe.reviews}></ReviewCard>
+              </div>
+            </React.Fragment>
+          ))}
+      </div>
+    </div>
+  )
 }
 
 export default Home
