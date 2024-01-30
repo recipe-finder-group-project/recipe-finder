9e74bd0d9284a74abddca7b7a040bfe976331de4
ervette <kiriltalalayko@gmail.com>
2024-01-30 12:35:04 +0000

    Added login feature using AUTH0 to login via google Not handling the logins with db or anything just now NOTE: Using env variables in react you have to start the env var name as: REACT_APP_
    For now google auth works

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import React from 'react';
 import Home from './pages/Home'
 import Navbar from './components/Navbar';
 
-
 function App() {
   return (
     <div className="App">
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,21 @@
-import { Link } from 'react-router-dom'
-import React from 'react'
-const Navbar = () => {
+import { Link } from "react-router-dom"
+import React from "react"
+import LoginButton from "./LoginButton"
+import LogoutButton from "./LogoutButton"
 
+const Navbar = () => {
   return (
     <header>
       <div className="container">
         <Link to="/">
           <h1>The Recipe Finder App ©</h1>
         </Link>
-          <Link to="/contact">
-              <p>Contact</p>
-          </Link>
+        <h1>{process.env.AUTH0_CLIENT_ID}</h1>
+        <LoginButton />
+        <LogoutButton />
+        <Link to="/contact">
+          <p>Contact</p>
+        </Link>
       </div>
     </header>
   )
