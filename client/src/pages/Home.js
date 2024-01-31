8703031126178325e7240fb0a8c6b71d3bbc81db
Adria Nykiel <adrian.nykiel@gmail.com>
2024-01-31 20:02:15 +0000

    api path sorted

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
   // const apiURL = "https://recipe-finder-server-f153.onrender.com/api/recipes"
   useEffect(() => {
     const fetchRecipes = async () => {
-      const response = await fetch("/api/recipes")
+      const response = await fetch("https://recipe-finder-server-f153.onrender.com/api/recipes/api/recipes")
       const json = await response.json()
 
       if (response.ok) {
