584e8c79023f8ec921fbd40e77f94a40a61fafb2
ervette <kiriltalalayko@gmail.com>
2024-01-31 16:38:45 +0000

    prettier

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,25 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import React from "react"
 
 //pages & components
-import Home from './pages/Home'
-import Navbar from './components/Navbar';
-import Profile from './components/Profile';
+import Home from "./pages/Home"
+import Navbar from "./components/Navbar"
+import Profile from "./components/Profile"
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar />
-      <Profile />
-        <div className='pages'>
+        <Navbar />
+        <Profile />
+        <div className="pages">
           <Routes>
-            <Route 
-            path='/'
-            element={<Home />}
-            />
+            <Route path="/" element={<Home />} />
           </Routes>
         </div>
       </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
