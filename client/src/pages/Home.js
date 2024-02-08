594cb82f8f868c739a806aa31b6a5f9fa07e1bb1
ervette <kiriltalalayko@gmail.com>
2024-02-08 18:51:24 +0000

    single recipe display p.s/ for future reference we wont be able to display all the dishes at once with their pictures cause of mongodb free tier

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,23 +3,38 @@ import ReviewCard from "../components/ReviewCard"
 import "../index.css"
 
 const Home = () => {
-  const [recipes, setRecipes] = useState(null)
+  // const [recipes, setRecipes] = useState(null)
+  // useEffect(() => {
+  //   const fetchRecipes = async () => {
+  //     const response = await fetch("http://localhost:5050/api/recipes")
+  //     const json = await response.json()
+
+  //     if (response.ok) {
+  //       setRecipes(json)
+  //     }
+  //   }
+  //   fetchRecipes()
+  // }, [])
+
+  const [recipe, setRecipe] = useState(null)
   useEffect(() => {
-    const fetchRecipes = async () => {
-      const response = await fetch("http://localhost:5050/api/recipes")
+    const fetchRecipe = async () => {
+      const response = await fetch(
+        "http://localhost:5050/api/recipes/65c0e9bb71e1c05cb56ae676"
+      )
       const json = await response.json()
 
       if (response.ok) {
-        setRecipes(json)
+        setRecipe(json)
       }
     }
-    fetchRecipes()
+    fetchRecipe();
   }, [])
 
   return (
     <div className="home">
       <div className="recipes">
-        {recipes &&
+        {/* {recipes &&
           recipes.map((recipe) => (
             <React.Fragment key={recipe._id}>
               <h2>{recipe.name}</h2>
@@ -45,7 +60,31 @@ const Home = () => {
                 <ReviewCard reviews={recipe.reviews}></ReviewCard>
               </div>
             </React.Fragment>
-          ))}
+          ))} */}
+
+        {recipe && (
+          <React.Fragment key={recipe._id}>
+            <h2>{recipe.name}</h2>
+            <img
+              className="recipeImage"
+              src={`data:image/jpeg;base64,${recipe.image.toString("base64")}`}
+              alt={"afs"}
+            />
+            <p>Diet Type - {recipe.dietType}</p>
+            <p>Meal Category - {recipe.mealCategory}</p>
+            <p>Preparation time - {recipe.preparationTime} min</p>
+            <p>Difficulty - {recipe.difficulty}*</p>
+            <p>Ingredients : </p>
+            <ul>
+              {recipe.ingredients.map((ingredient, index) => (
+                <li key={index}>{ingredient}</li>
+              ))}
+            </ul>
+            <div>
+              <ReviewCard reviews={recipe.reviews}></ReviewCard>
+            </div>
+          </React.Fragment>
+        )}
       </div>
     </div>
   )
