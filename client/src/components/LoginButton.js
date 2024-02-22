141cd0eb47daa2e9a1882522bb0635f6fb032a52
ervette <kiriltalalayko@gmail.com>
2024-02-22 12:40:51 +0000

    created the working & resonsive navbar

--- client/src/components/LoginButton.js
+++ client/src/components/LoginButton.js
@@ -5,7 +5,7 @@ const LoginButton = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
 
   return (
-    !isAuthenticated && <button onClick={() => loginWithRedirect()}>Sign In</button>
+    !isAuthenticated && <button className="login-button" onClick={() => loginWithRedirect()}>Sign In</button>
   )
 }
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,23 +1,39 @@
 import { Link } from "react-router-dom"
 import React from "react"
+import { Navbar, Nav } from "react-bootstrap"
 import LoginButton from "./LoginButton"
 import LogoutButton from "./LogoutButton"
+import "../index.css"
 
-const Navbar = () => {
+const NavbarComponent = () => {
   return (
-    <header>
-      <div className="container">
-        <Link to="/">
-          <h1>The Recipe Finder App ©</h1>
-        </Link>
+    <Navbar
+      className="rounded-pill d-flex justify-content-between align-items-center"
+      fixed="top"
+      style={{
+        backgroundColor: "rgba(96, 96, 96, 0.5)",
+        backdropFilter: "blur(10px)",
+        padding: "10px",
+      }}
+    >
+      <Navbar.Brand className="gradient-text">Healthy</Navbar.Brand>
+      <Nav className="mr-auto justify-content-center">
+        <Nav.Link href="#" className="links-text">
+          Recipe Finder
+        </Nav.Link>
+        <Nav.Link href="#" className="links-text">
+          About Us
+        </Nav.Link>
+        <Nav.Link href="#" className="links-text">
+          Contacts
+        </Nav.Link>
+      </Nav>
+      <div>
         <LoginButton />
         <LogoutButton />
-        <Link to="/contact">
-          <p>Contact</p>
-        </Link>
       </div>
-    </header>
+    </Navbar>
   )
 }
 
-export default Navbar;
+export default NavbarComponent
