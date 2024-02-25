b6909de227a2f58128c69dbe8ad8f3f8d82d8611
ervette <kiriltalalayko@gmail.com>
2024-02-25 10:34:30 +0000

    woking navbar + banner except of auto scroll to finder component

---
+++ client/src/components/Banner.js
@@ -0,0 +1,86 @@
+import React from "react"
+import { Button, Card, Carousel } from "react-bootstrap"
+import mediimage from "../images/medi-lunch.png"
+import veganimage from "../images/vegan-lunch.png"
+import veggieimage from "../images/veggie-lunch.png"
+import "./BannerComponent.css" // Make sure your CSS file is named accordingly
+
+const BannerComponent = () => {
+  return (
+    <div className="container-fluid p-0 d-flex" style={{ height: "400px" }}>
+      <div
+        className="d-flex flex-column justify-content-between"
+        style={{ flex: "1" }}
+      >
+        <Card className="rounded-15" style={{ flexGrow: "1" }}>
+          <Card.Body className="d-flex flex-column">
+            <Card.Title className="title-text">Healthy Food</Card.Title>
+            <Card.Title className="title-text">Recipe Finder</Card.Title>
+            <div style={{paddingTop:"10px"}}>
+              <Card.Subtitle className="text-formation">
+                Welcome to Recipe
+              </Card.Subtitle>
+              <Card.Subtitle className="text-formation">
+                Finder - Your Adventure in
+              </Card.Subtitle>
+              <Card.Subtitle className="text-formation">
+                Cooking Begins Here!
+              </Card.Subtitle>
+            </div>
+          </Card.Body>
+        </Card>
+        <div className="d-flex">
+          <Button
+            className="rounded-15 btn-gap btn-start"
+            style={{ width: "50%" }}
+          >
+            Start Now
+          </Button>
+          <Button
+            className="rounded-15 btn-arrow"
+            style={{ width: "50%" }}
+          >
+            ↙
+          </Button>
+        </div>
+      </div>
+      <div className="carousel-container" style={{ flex: "1" }}>
+        <Carousel fade className="rounded-15 h-100">
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={veganimage}
+              alt="Vegan Lunches"
+            />
+            <Carousel.Caption>
+              <h2 style={{ textAlign: "left" }}>Vegan Lunches</h2>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={mediimage}
+              alt="Mediterranean Creations"
+            />
+            <Carousel.Caption>
+              <h2 style={{ textAlign: "left" }}>Mediterranean Creations</h2>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={veggieimage}
+              alt="Vegetarian tastes"
+            />
+            <Carousel.Caption>
+              <h2 style={{ textAlign: "left" }}>Vegetarian tastes</h2>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    </div>
+  )
+}
+
+export default BannerComponent
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,34 +1,35 @@
-import React, { useState, useEffect } from "react";
-import RecipeCard from "../components/Recipe";
-import DietTypeFilter from "../components/dietTypeFilter";
-import MealCategoryFilter from "../components/mealCategoryFilter";
+import React, { useState, useEffect } from "react"
+import { Spinner } from "react-bootstrap"
+import RecipeCard from "../components/Recipe"
+import DietTypeFilter from "../components/dietTypeFilter"
+import MealCategoryFilter from "../components/mealCategoryFilter"
+import Banner from "../components/Banner"
 
 const Home = () => {
-  const [mainFilter, setMainFilter] = useState(null);
-  const [subFilter, setSubFilter] = useState(null);
-  const [recipes, setRecipes] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [mainFilter, setMainFilter] = useState(null)
+  const [subFilter, setSubFilter] = useState(null)
+  const [recipes, setRecipes] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
 
   const handleMainFilterSelect = (filter) => {
-    setMainFilter(filter);
-  };
+    setMainFilter(filter)
+  }
 
   const handleSubFilterSelect = (type, filter) => {
-    setSubFilter(filter);
-    // setIsLoading(true);
-  };
+    setSubFilter(filter)
+  }
 
   useEffect(() => {
-    console.log(`Sub filter updated: ${subFilter}`);
-  }, [subFilter]);
+    console.log(`Sub filter updated: ${subFilter}`)
+  }, [subFilter])
 
   useEffect(() => {
     // Fetch recipes when mainFilter changes
     if (mainFilter !== null && subFilter !== null) {
-      setIsLoading(true);
-      sendRecipeRequest(mainFilter, subFilter);
+      setIsLoading(true)
+      sendRecipeRequest(mainFilter, subFilter)
     }
-  }, [mainFilter, subFilter]);
+  }, [mainFilter, subFilter])
 
   const sendRecipeRequest = async (mainFilter, subFilter) => {
     try {
@@ -41,23 +42,25 @@ const Home = () => {
           dietTypeFilter: mainFilter,
           mealCategoryFilter: subFilter,
         }),
-      });
+      })
 
       if (!response.ok) {
-        throw new Error("Failed to fetch recipes");
+        throw new Error("Failed to fetch recipes")
       }
-      const responseData = await response.json();
-      console.log(responseData);
-      setRecipes(responseData);
+      const responseData = await response.json()
+      console.log(responseData)
+      setRecipes(responseData)
     } catch (error) {
-      console.error("Error fetching recipes:", error);
+      console.error("Error fetching recipes:", error)
     } finally {
-      setIsLoading(false);
+      setIsLoading(false)
     }
-  };
+  }
 
   return (
     <div className="home">
+      <Banner />
+
       <DietTypeFilter onSelect={handleMainFilterSelect} />
       {mainFilter && (
         <MealCategoryFilter
@@ -65,9 +68,22 @@ const Home = () => {
           onSelect={handleSubFilterSelect}
         />
       )}
-      {isLoading ? <h1>Loading...</h1> : recipes && <RecipeCard recipes={recipes} />}
+      {isLoading ? (
+        <center>
+          <Spinner
+            style={{ marginTop: "50px" }}
+            animation="border"
+            role="status"
+            variant="warning"
+          >
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </center>
+      ) : (
+        recipes && <RecipeCard recipes={recipes} />
+      )}
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
