d4162b8d53f123d2d6326b1d993042ebeb477b34
ervette <kiriltalalayko@gmail.com>
2024-02-26 16:52:55 +0000

    checking authentication

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,49 @@
 // import { Link } from "react-router-dom"
-import React from "react"
+import React, { useEffect } from "react"
+import { useAuth0 } from "@auth0/auth0-react"
 import { Navbar, Nav } from "react-bootstrap"
 import LoginButton from "./LoginButton"
 import LogoutButton from "./LogoutButton"
 import "../index.css"
 
 const NavbarComponent = () => {
+  const { user, isAuthenticated } = useAuth0()
+
+  useEffect(() => {
+    // Call the function to send data to the backend when the component mounts
+    if (isAuthenticated) {
+      sendDataToBackend(user)
+    }
+  }, [isAuthenticated, user])
+
+  const sendDataToBackend = async (userData) => {
+    try {
+      const response = await fetch(
+        "https://recipe-finder-server-xgd5.onrender.com/save-user-data",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            // Add any other headers you may need, such as authorization tokens
+          },
+          body: JSON.stringify({
+            name: userData.name,
+            email: userData.email,
+          }),
+        }
+      )
+
+      if (!response.ok) {
+        throw new Error("Failed to save user data")
+      }
+
+      // Handle the response from the server if needed
+      const responseData = await response.json()
+      console.log("User data saved successfully", responseData)
+    } catch (error) {
+      console.error("Error saving user data:", error)
+    }
+  }
 
   const scrollToRecipeFinder = () => {
     const navbarHeight = 95
@@ -31,7 +69,11 @@ const NavbarComponent = () => {
     >
       <Navbar.Brand className="gradient-text">Healthy</Navbar.Brand>
       <Nav className="mr-auto justify-content-center">
-        <Nav.Link href="#" className="links-text" onClick={scrollToRecipeFinder}>
+        <Nav.Link
+          href="#"
+          className="links-text"
+          onClick={scrollToRecipeFinder}
+        >
           Recipe Finder
         </Nav.Link>
         <Nav.Link href="#" className="links-text">
@@ -42,6 +84,7 @@ const NavbarComponent = () => {
         </Nav.Link>
       </Nav>
       <div>
+        {isAuthenticated && <h1>{user.name}</h1>}
         <LoginButton />
         <LogoutButton />
       </div>
